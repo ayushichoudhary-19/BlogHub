cbdc8a3cc1db78d0332ed9bb800388a7beecdfc7
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-02-05 11:09:06 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
+    appwriteUrl: process.env.VITE_APPWRITE_URL,
+    appwriteProjectId: process.env.VITE_APPWRITE_PROJECT_ID,
+    appwriteDatabaseId: process.env.VITE_APPWRITE_DATABASE_ID,
+    appwriteCollectionId: process.env.VITE_APPWRITE_COLLECTION_ID,
+    appwriteBucketId: process.env.VITE_APPWRITE_BUCKET_ID,
+};
 
-export default conf
+export default conf;
